0ef2fb9a6ec7762269adc9d321e587276be5cf1e
rafalb8 <rafalb8@hotmail.com>
2019-03-26 16:54:20 +0100

    Updated MovieEntry

--- src/main/java/com/movtrack/MovieEntry.java
+++ src/main/java/com/movtrack/MovieEntry.java
@@ -1,16 +1,27 @@
 package com.movtrack;
 
 import com.movtrack.RestClient.Search;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 
 public class MovieEntry extends HorizontalLayout {
+    private Image imgPoster;
+    private VerticalLayout vlInfo;
     private Label lblTitle;
+    private Label lblType;
+
 
     public MovieEntry(Search movie) {
-        lblTitle = new Label(movie.getTitle());
+        imgPoster = new Image(movie.getPoster(), movie.getPoster());
+        vlInfo = new VerticalLayout();
+        lblTitle = new Label(movie.getTitle() + " (" + movie.getYear() +")");
+        lblType = new Label(movie.getType().toUpperCase());
 
-        add(lblTitle);
+
+        vlInfo.add(lblTitle);
+        add(imgPoster, vlInfo, lblType);
     }
 }
