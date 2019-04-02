dc528377383e352f64c24b1b704258054ead409b
rafalb8 <rafalb8@hotmail.com>
2019-04-02 17:39:45 +0200

    Changed RestClientTest

--- src/main/java/com/movtrack/Test/RestClientTest.java
+++ src/main/java/com/movtrack/Test/RestClientTest.java
@@ -2,35 +2,29 @@ package com.movtrack.Test;
 
 import com.movtrack.RestClient.Movie;
 import com.movtrack.RestClient.SearchResult;
+import com.movtrack.RestClient.RestClient;
 import org.junit.jupiter.api.Test;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RestClientTest {
 
-    private Client client;
-    private WebTarget webtarget;
+    private RestClient restClient;
 
     private RestClientTest() {
-        client = ClientBuilder.newClient();
-        webtarget = client.target("http://www.omdbapi.com/").queryParam("apikey", "b45ac754");
+        restClient = RestClient.getInstance();
     }
 
     @Test
     void idRequest(){
-        Movie m = webtarget.queryParam("i", "tt3896198").request(MediaType.APPLICATION_JSON).get(Movie.class);
+        Movie m = restClient.getMovieByID("tt3896198");
 
         assertEquals("Guardians of the Galaxy Vol. 2", m.getTitle(), "Incorrect title");
     }
 
     @Test
     void searchRequest() {
-        SearchResult s = webtarget.queryParam("s", "avengers").request(MediaType.APPLICATION_JSON).get(SearchResult.class);
+        SearchResult s = restClient.searchMovieByTitle("avengers");
 
         assertEquals("The Avengers", s.getSearch().get(0).getTitle() , "Incorrect title");
     }
