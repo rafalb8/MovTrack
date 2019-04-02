72abdde6cc8c1007c8905ea556fb33a05e494e52
rafalb8 <rafalb8@hotmail.com>
2019-04-02 18:16:37 +0200

    Changed theme to dark

--- src/main/java/com/movtrack/Views/MainView.java
+++ src/main/java/com/movtrack/Views/MainView.java
@@ -8,7 +8,10 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
+@Theme(value = Lumo.class, variant = Lumo.DARK)
 @Route("")
 @PWA(name = "MovTrack - Track watched Movies and TV Shows", shortName = "MovTrack")
 public class MainView extends VerticalLayout {
