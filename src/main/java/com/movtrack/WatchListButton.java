e1498f3b42ec97c3e6d05a1f650382819ea325fc
rafalb8 <rafalb8@hotmail.com>
2019-04-13 18:11:51 +0200

    Fixed WatchListButton

--- src/main/java/com/movtrack/WatchListButton.java
+++ src/main/java/com/movtrack/WatchListButton.java
@@ -6,15 +6,31 @@ import com.vaadin.flow.component.icon.VaadinIcon;
 
 // Button to add/remove movie to WatchList
 public class WatchListButton extends Button {
-    private static final Icon icoAdd = new Icon(VaadinIcon.CHECK_SQUARE_O);
-    private static final Icon icoRemove = new Icon(VaadinIcon.CHECK_SQUARE);
+    private final Icon icoAdd = new Icon(VaadinIcon.CHECK_SQUARE_O);
+    private final Icon icoRemove = new Icon(VaadinIcon.CHECK_SQUARE);
+    private boolean boolWatched;
 
     public WatchListButton() {
-        super("Add/Remove Movie", icoAdd);
-        addClickListener(click -> clickEvent());
+        super();
+
+        // Add click listener
+        addClickListener(click -> refresh());   // TODO: replace refresh with clik event
+
+        // Get status from watchlist
+        boolWatched = false;
+
+        refresh();
     }
 
-    private void clickEvent(){
-        setIcon(icoRemove);
+    private void refresh(){
+        if(!boolWatched){
+            setIcon(icoAdd);
+            setText("Add Movie");
+        } else {
+            setIcon(icoRemove);
+            setText("Remove Movie");
+        }
+
+        boolWatched = !boolWatched;
     }
 }
