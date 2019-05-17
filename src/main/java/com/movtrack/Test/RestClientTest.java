be9bfa445f1809a654f46c0e4321a6f1c7c82f47
rafalb8 <rafalb8@hotmail.com>
2019-05-17 13:08:01 +0200

    Added Tv Show by id test

--- src/main/java/com/movtrack/Test/RestClientTest.java
+++ src/main/java/com/movtrack/Test/RestClientTest.java
@@ -3,6 +3,7 @@ package com.movtrack.Test;
 import com.movtrack.RestClient.Movie.Movie;
 import com.movtrack.RestClient.RestClient;
 import com.movtrack.RestClient.Search.Search;
+import com.movtrack.RestClient.TV.TV;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,15 +17,23 @@ class RestClientTest {
     }
 
     @Test
-    void idRequest(){
+    void movieByIdRequest(){
         Movie m = restClient.getMovieByID("550");
 
         assertEquals("Fight Club", m.getTitle(), "Incorrect title");
     }
 
     @Test
+    void tvShowByIdRequest(){
+        TV m = restClient.getTVShowByID("63926");
+
+        assertEquals("One-Punch Man", m.getName(), "Incorrect name");
+    }
+
+    @Test
     void searchRequest() {
         Search s = restClient.searchByTitle("avengers");
+
         assertNotNull(s.getResults(),"Search is empty");
         assertEquals("Avengers: Endgame", s.getResults().get(0).getTitle() , "Incorrect title");
     }
