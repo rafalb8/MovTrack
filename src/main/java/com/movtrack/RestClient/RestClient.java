c35cb9077a6783f7434c15b8a495c0573ef5e758
rafalb8 <rafalb8@hotmail.com>
2019-05-17 13:28:42 +0200

    Added Support for Movie and Tv Show recommendations.

--- src/main/java/com/movtrack/RestClient/RestClient.java
+++ src/main/java/com/movtrack/RestClient/RestClient.java
@@ -1,7 +1,9 @@
 package com.movtrack.RestClient;
 
 import com.movtrack.RestClient.Movie.Movie;
+import com.movtrack.RestClient.Movie.Recommendation.MovieRecommendations;
 import com.movtrack.RestClient.Search.Search;
+import com.movtrack.RestClient.TV.Recommendation.TvShowRecommendations;
 import com.movtrack.RestClient.TV.TvShow;
 
 import javax.ws.rs.client.Client;
@@ -33,16 +35,29 @@ public class RestClient {
         return singleton;
     }
 
+    // Get information about movie
     public Movie getMovieByID(String id){
         return webtarget.path("/movie/" + id).request(MediaType.APPLICATION_JSON).get(Movie.class);
     }
 
+    // Get information about Tv Show
     public TvShow getTVShowByID(String id){
         return webtarget.path("/tv/" + id).request(MediaType.APPLICATION_JSON).get(TvShow.class);
     }
 
+    // Search for movie or tv show
     public Search searchByTitle(String title){
         return webtarget.path("/search/multi").queryParam("query", title).request(MediaType.APPLICATION_JSON).get(Search.class);
+    }
+
+    // Get Movie Recommendations
+    public MovieRecommendations getMovieRecommendations(String id){
+        return webtarget.path("/movie/" + id +"/recommendations").request(MediaType.APPLICATION_JSON).get(MovieRecommendations.class);
+    }
+
+    // Get Tv Show Recommendations
+    public TvShowRecommendations getTvShowRecommendations(String id){
+        return webtarget.path("/tv/" + id +"/recommendations").request(MediaType.APPLICATION_JSON).get(TvShowRecommendations.class);
     }
 
 }
