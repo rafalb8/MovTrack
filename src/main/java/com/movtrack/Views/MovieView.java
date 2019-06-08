604c285d8dbbeb757c77969b2579afd4836e1f46
rafalb8 <rafalb8@hotmail.com>
2019-06-08 13:12:06 +0200

    Added imdb link

--- src/main/java/com/movtrack/Views/MovieView.java
+++ src/main/java/com/movtrack/Views/MovieView.java
@@ -4,6 +4,7 @@ import com.movtrack.Banner;
 import com.movtrack.List.WatchListButton;
 import com.movtrack.List.WatchedListButton;
 import com.movtrack.MediaBar.MediaBar;
+import com.movtrack.RestClient.Movie.Genre;
 import com.movtrack.RestClient.Movie.Movie;
 import com.movtrack.RestClient.RestClient;
 import com.vaadin.flow.component.html.Image;
@@ -22,6 +23,7 @@ import javax.annotation.PostConstruct;
 public class MovieView extends VerticalLayout implements HasUrlParameter<String> {
 
     private RestClient restClient;
+    private String imdbID;
 
     // Design
     private final Banner banner;
@@ -33,6 +35,7 @@ public class MovieView extends VerticalLayout implements HasUrlParameter<String>
     private final Label lblTitle;
     private final Label lblGenre;
     private final Label lblPlot;
+    private final Label lblVotes;
     private final MediaBar recommended;
 
     @Autowired
@@ -54,7 +57,11 @@ public class MovieView extends VerticalLayout implements HasUrlParameter<String>
         lblTitle = new Label();
         lblGenre = new Label();
         lblPlot = new Label();
+        lblVotes = new Label();
         recommended = new MediaBar("Recommendation");
+
+        // Add click event to title
+        lblTitle.getElement().addEventListener("click", event -> goToIMDB());
 
 
         setDefaultHorizontalComponentAlignment(Alignment.STRETCH);
@@ -66,7 +73,7 @@ public class MovieView extends VerticalLayout implements HasUrlParameter<String>
 
         hlMainInfo.add(imgPoster, vlInfo);
         hlTitle.add(lblTitle);
-        vlInfo.add(hlTitle, lblGenre, lblPlot);
+        vlInfo.add(hlTitle, lblVotes, lblGenre, lblPlot);
         vlInfo.getElement().getStyle().set("background", "#E7EBEF");
 
         add(banner, hlMainInfo, hlButtons, recommended);
@@ -87,9 +94,28 @@ public class MovieView extends VerticalLayout implements HasUrlParameter<String>
             imgPoster.setSrc("https://image.tmdb.org/t/p/w300" + movie.getPosterPath());
         }
 
+        // Set Title label
         lblTitle.getElement().setProperty("innerHTML","<h1>"+movie.getTitle() + (movie.getReleaseDate() == null?"":" (" + movie.getReleaseDate() + ")") +"</h1>");
-        lblGenre.getElement().setProperty("innerHTML","<b>Genres: " + movie.getGenres().get(0).getName()+"</b>");
+
+        // Build string with all genres
+        if(!movie.getGenres().isEmpty()){
+            StringBuilder genres = new StringBuilder();
+
+            for(Genre genre : movie.getGenres()){
+                if(genre != movie.getGenres().get(0)){
+                    genres.append(", ");
+                }
+                genres.append(genre.getName());
+            }
+
+            lblGenre.getElement().setProperty("innerHTML","<b>Genres: " + genres.toString() +"</b>");
+        }
+
+        // Set plot label
         lblPlot.getElement().setProperty("innerHTML","<i>"+movie.getOverview()+"</i>");
+
+        // Set votes label
+        lblVotes.getElement().setProperty("innerHTML", "<b> " + (int)(movie.getVoteAverage() * 10) + " % | " + movie.getVoteCount() + " votes</b>");
 
         // Update buttons
         btnWatched.init("movie", movie.getId());
@@ -98,6 +124,9 @@ public class MovieView extends VerticalLayout implements HasUrlParameter<String>
         // Recommendation list
         recommended.setTitle("If you like " + movie.getTitle() + ", check out...");
         recommended.showRecommended(movie.getId(), "movie");
+
+        // Save imdb ID
+        imdbID = movie.getImdbId();
     }
 
     @Override
@@ -106,5 +135,9 @@ public class MovieView extends VerticalLayout implements HasUrlParameter<String>
         if(parameter.chars().allMatch(Character::isDigit)) {
             refreshInfo(restClient.getMovieByID(parameter));
         }
+    }
+
+    private void goToIMDB(){
+        getUI().ifPresent(ui -> ui.getPage().executeJavaScript("window.open(\"http://imdb.com/title/"+imdbID+"\", \"_self\");"));
     }
 }
