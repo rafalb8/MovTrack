a119faf69cb3e46d93af7e10085ba6b9ddf998ea
rafalb8 <rafalb8@hotmail.com>
2019-06-08 13:54:42 +0200

    Added notification when adding movie to list

--- src/main/java/com/movtrack/List/WatchListButton.java
+++ src/main/java/com/movtrack/List/WatchListButton.java
@@ -2,6 +2,7 @@ package com.movtrack.List;
 
 import com.movtrack.List.DB.ListManager;
 import com.movtrack.List.DB.MediaEntity;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,8 +33,10 @@ public class WatchListButton extends ListButton {
     private void clickEvent(){
         if(!boolAdded){
             listManager.save(new MediaEntity(mediaID, mediaType, listType));
+            Notification.show("Added to Watched list");
         } else {
             listManager.deleteByID(ID);
+            Notification.show("Removed from Watched list");
         }
 
         boolAdded = !boolAdded;
--- src/main/java/com/movtrack/List/WatchedListButton.java
+++ src/main/java/com/movtrack/List/WatchedListButton.java
@@ -2,6 +2,7 @@ package com.movtrack.List;
 
 import com.movtrack.List.DB.ListManager;
 import com.movtrack.List.DB.MediaEntity;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,8 +33,10 @@ public class WatchedListButton extends ListButton {
     private void clickEvent(){
         if(!boolAdded){
             listManager.save(new MediaEntity(mediaID, mediaType, listType));
+            Notification.show("Added to Watchlist");
         } else {
             listManager.deleteByID(ID);
+            Notification.show("Removed from Watchlist");
         }
 
         boolAdded = !boolAdded;
